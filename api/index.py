284f12e269b2647ac002a6438bc966a3bd2b9deb
flukyape <141446648+flukyape@users.noreply.github.com>
2023-08-05 06:47:13 +0000

    server

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/api/python")
+@app.route("/api/python", methods=['POST'])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    data = request.get_json()
+    print(data)
+    return {"message": "Received"}, 200
+
+if __name__ == "__main__":
+    print("Starting Flask server...")
+    app.run(debug=True)  # run the server in debug mode
