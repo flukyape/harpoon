95a99caf5241ae6cd6ba890506772630cad0a6ef
flukyape <141446648+flukyape@users.noreply.github.com>
2023-08-05 07:42:42 +0000

    200 please

--- api/index.py
+++ api/index.py
@@ -2,38 +2,19 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-def find_hits(data):
-    result = []
-    transactions = data['block']['transactions']
-    for transaction in transactions:
-        created_contract = transaction.get('createdContract')
-        if created_contract is not None:
-            from_address = transaction['from']['address']
-            contract_address = created_contract['address']
-            result.append({
-                'from_address': from_address,
-                'contract_address': contract_address
-            })
-    return result
-
-@app.route('/find_hits', methods=['POST'])
-def find_hits_endpoint():
-    data = request.json
-    hits = find_hits(data)
+@app.route("/api/python", methods=['POST'])
+def process_data():
+    data = request.get_json()
+    transactions = data.get('event', {}).get('data', {}).get('block', {}).get('transactions', [])
     
-    if not hits:
-        message = "There's nothing in this block"
-        print(message)
-        return jsonify(message=message), 200
+    # Extracting smart contract addresses
+    smart_contract_addresses = [tx['from']['address'] for tx in transactions if tx.get('createdContract')]
 
-    response_message = f"{len(hits)} number of smart contracts"
-    print(response_message)
-    print('Contracts:', hits)
-    response = {
-        'message': response_message,
-        'contracts': hits
-    }
-    return jsonify(response), 200
+    print('Smart contract addresses:')
+    print(smart_contract_addresses)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    return jsonify({"message": "Received", "smart_contract_addresses": smart_contract_addresses}), 200
+
+if __name__ == "__main__":
+    print("Starting Flask server...")
+    app.run(debug=True)  # run the server in debug mode
