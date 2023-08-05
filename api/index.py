8e938ed85faa168149cc59b03a6bf6b0c829a3f5
flukyape <141446648+flukyape@users.noreply.github.com>
2023-08-05 07:56:54 +0000

    damn

--- api/index.py
+++ api/index.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 def hello_world():
     data = request.get_json()
     print('data is here')
-    print(data.event)
+    print(data)
+    print('done')
 
     return {"message": "Received"}, 200
 
