52040514f33aa6a88398e10ce11b097ad565c7bf
flukyape <141446648+flukyape@users.noreply.github.com>
2023-08-05 07:45:55 +0000

    goign back

--- api/index.py
+++ api/index.py
@@ -1,19 +1,13 @@
-from flask import Flask, request, jsonify
-
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/api/python", methods=['POST'])
-def process_data():
+def hello_world():
     data = request.get_json()
-    transactions = data.get('event', {}).get('data', {}).get('block', {}).get('transactions', [])
-    
-    # Extracting smart contract addresses
-    smart_contract_addresses = [tx['from']['address'] for tx in transactions if tx.get('createdContract')]
+    print('data is here')
+    print(data)
 
-    print('Smart contract addresses:')
-    print(smart_contract_addresses)
-
-    return jsonify({"message": "Received", "smart_contract_addresses": smart_contract_addresses}), 200
+    return {"message": "Received"}, 200
 
 if __name__ == "__main__":
     print("Starting Flask server...")
